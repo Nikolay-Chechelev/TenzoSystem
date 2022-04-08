5b70eac18b8ec30b341d7dd0bba04b2dbc9a0fdb
Nikolay Chechelev <nikolaychechelev@gmail.com>
2022-04-08 22:16:01 +0400

    filtering and round drawing

--- csv_read.py
+++ csv_read.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from time import sleep
 import pygame
 
-f = open('samples/f_sample_933338.csv', 'r')
+f = open('samples/Good_amort_50hz.csv', 'r')
 
 csv_data = csv.reader(f, delimiter=';')
 data = []
@@ -24,11 +24,54 @@ index_p = []
 index_n = []
 cross = 0
 
-for i in range(len(data) - 1):
-    if data[i] * data[i+1] < 0:
-        index_p.append(cross + (i - cross) // 2)
-        cross = i
+def fix_array(array):
+    l1 = 0
+    l2 = 0
+    x = 0
+    i = 0
+    d = 0
 
+    while array[i] * array[i + 1] >= 0 and x == 0:
+        i += 1
+    x = i - x
+    i += 1
+
+    while i < len(array)-5000:
+        x = i
+        i += 1
+        while array[i] * array[i + 1] >= 0:
+            i += 1
+        l1 = i - x
+        x = i
+        i += 1
+        print('1 halfperiod =', l1)
+
+        while array[i] * array[i + 1] >= 0:
+            i += 1
+        l2 = i - x
+        print('2 halfperiod =', l2)
+
+        if l1 == l2:
+            pass
+        if l1 > l2:
+            d = l1 // (l1 - l2)
+            m = l1 - l2
+            while m > 0:
+                del array[i - l1 + d * m]
+                m -= 1
+            i -= l1 - l2
+
+        if l1 < l2:
+            d = l2 // (l2 - l1)
+            m = l2 - l1
+            while m > 0:
+                del array[i - l2 + d * m]
+                m -= 1
+            i -= l2 - l1
+        sleep(0.1)
+    return array
+
+data = fix_array(data)
 plt.plot(data)
 plt.show()
 
@@ -36,25 +79,14 @@ display = pygame.display
 screen = display.set_mode([1000, 1000])
 done = None
 x = 500
-y = -1
-m = -1
-t = (max(data) + min(data)) // 2
-t = -11.5
-print(t, max(data), min(data))
-for i in range(len(data)):
+c = 1
+for i in range(len(data) - 2):
     pygame.draw.line(screen, [100, 100, 100], [x, data[i]*5 + 500], [x, data[i + 1]*5 + 500])
     pygame.draw.line(screen, [100, 0, 100], [i, data[i]*5 + 500], [i + 1, data[i + 1]*5 + 500])
-    print(i)
-    # if i in index_p:
-    #     print('p', i)
-    #     y *= -1
-    # if (data[i] * data[i+1]) < 0:
-    #     m *= -1
-    # x += m
-    if data[i] > t:
-        x += 1
-    else:
-        x -= 1
+    if data[i+1] * data[i] < 0:
+        c *= -1
+    x += c
+
 
 
     display.flip()
